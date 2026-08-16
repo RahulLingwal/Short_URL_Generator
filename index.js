32afc3ca4eab67fba6d6413c237f674de0d504f0
Rahul Lingwal <rahullingwal200@gmail.com>
2026-08-16 20:33:43 +0530

    refactor: modularize express application configuration into a separate app.js file

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-
 const { connectDB } = require("./dbconnection.js");
-const URL = require("./models/url.models.js");
-const { requireAuth, checkAuth } = require("./middlewares/auth.middleware.js");
+const { app } = require("./app.js");
 
-const urlRoute = require("./routers/url.routes.js");
-const staticRoute = require("./routers/static.routes.js");
-const userRoute = require("./routers/user.routes.js");
-const redirectRoute = require("./routers/redirect.routes.js");
-
-const app = express();
 const PORT = 5000;
-
-app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
-
-// middlewares
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-
-// routes
-app.use("/url", requireAuth, urlRoute); // Forward all requests starting with "/url" to the URL router
-app.use("/user", userRoute);
-app.use("/", checkAuth, staticRoute);
-app.use("/", redirectRoute);
 
 // database connection
 connectDB("mongodb://localhost:27017/short-url")
